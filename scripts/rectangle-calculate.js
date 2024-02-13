996df602dddf2756077fac7fc61fe75dedbec91d
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2024-02-14 02:08:22 +0600

    added calculate area of the rectangle

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,23 @@
-function calculateRectangleArea(){
-   console.log("added")
+function calculateRectangleArea() {
+    // get width of the rectangle area
+    const widthInput = document.getElementById("rectangle-width");
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+
+    console.log(width);
+
+    // get length of the rectangle area
+    const lengthInput = document.getElementById("rectangle-length");
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // calculate rectangle area
+    const area = width * length;
+    console.log("area of the rectangle is:", area);
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById("rectangle-area");
+    rectangleAreaSpan.innerText = area;
+
 }
